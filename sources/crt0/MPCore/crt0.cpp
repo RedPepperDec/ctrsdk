79dbe9e6b995138d4b9c01c13ea90b7d4a39e97a
Moddimation <vakhidmytr06@gmail.com>
2025-12-10 23:41:52 +0100

    nninit stuff, wip

--- sources/crt0/MPCore/crt0.cpp
+++ sources/crt0/MPCore/crt0.cpp
@@ -1,17 +1,25 @@
 #include <nn/svc/svc_Api.h>
+#include <rt_locale.h>
+#include <rt_sys.h>
 
 extern "C" void nninitRegion();
 extern "C" void nninitLocale();
 extern "C" void nninitSystem();
 extern "C" void nninitStartUp();
-extern "C" __weak void __cpp_initialize_aeabi_();
+
+extern "C" __weak void   __cpp_initialize_aeabi_();
+extern "C" unsigned int* __rt_locale( void );
+
 extern "C" void nninitCallStaticInitializers();
 extern "C" void nninitSetup();
 extern "C" void nnMain();
 
-// clang-format off
-extern "C" __asm void __ctr_start()   
+extern "C" unsigned char Image$$ZI$$ZI$$Base[];
+extern "C" unsigned char Image$$ZI$$ZI$$Limit[];
+
+extern "C" __asm void __ctr_start()
 {
+        // clang-format off
     bl __cpp(nninitRegion)
     bl __cpp(nninitLocale)
     bl __cpp(nninitSystem)
@@ -21,6 +29,27 @@ extern "C" __asm void __ctr_start()
     bl __cpp(nninitSetup)
     bl __cpp(nnMain)
     b __cpp(nn::svc::ExitProcess)
+        // clang-format on
 }
 
+extern "C" __asm void nninitRegion()
+{
+        // clang-format off
+    ldr r0, =__cpp(Image$$ZI$$ZI$$Base)
+    ldr r1, =__cpp(Image$$ZI$$ZI$$Limit)
+    mov r2, #0
+run
+    cmp r0, r1
+    strcc r2, [r0], #4
+    bcc run
+    bx lr
+        // clang-format on
+}
+
+extern "C" void nninitLocale()
+{
+        unsigned int* rt = __rt_locale();
+        *( rt + 1 )      = (unsigned int)_get_lc_ctype( 0, 0 ) + 1;
+        *( rt + 3 )      = (unsigned int)_get_lc_numeric( 0, 0 );
+}
 // clang-format on
